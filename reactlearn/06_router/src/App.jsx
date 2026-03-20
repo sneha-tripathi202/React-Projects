430dd3828b0cbda83d1d74d97162d4e759054477
Sneha Tripathi <st0744397@gmail.com>
2026-03-20 11:09:21 +0530

    rect router

--- reactlearn/06_router/src/App.jsx
+++ reactlearn/06_router/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { Navbar } from '.components/Home.jsx'
+import  Navbar  from '.components/Navbar.jsx'
 import Home from '.components/Home.jsx'
 import Footer from '.components/Footer'
 
--- reactlearn/Layout.jsx
+++ reactlearn/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import  Navbar  from './components/Footer.jsx'
+import  Navbar  from './components/Navbar.jsx'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer.jsx'
 
